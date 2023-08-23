276a91f13ed5bcb8817584707c6954e8210df929
窪江健 <cigarq@gmail.com>
2023-08-23 17:41:35 +0900

    0823卒業制作

--- shorten.py
+++ shorten.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, send_file, request
+import keepa
+import datetime
+import plotly.graph_objs as go
 import re
+from bs4 import BeautifulSoup
+import requests  
+import io
+import math
+
 
 app = Flask(__name__)
+
+# アクセスキーを設定してKeepa APIを初期化
+access_key = '37da9spj5dk348f7l4nmuf2vgt1v1tsin8vjm3jcb8594jjvst0ui4ii81l0i08r'
+api = keepa.Keepa(access_key)
+
+# ASIN = 'B08C74XB4Q'
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -9,27 +23,151 @@ def index():
         amazon_link = request.form.get("amazon_link")
         shortened_link = shorten_amazon_link(amazon_link)
         return jsonify(shortened_link=shortened_link)
-    
-
-
     return render_template("index.html")
 
-##「dp以下の英数字（ASIN）」を「amazon.co.jp/dp/」の後ろにつけて返す
+@app.route('/get-image/<ASIN>')
+def image_endpoint(ASIN):
+    try:
+        image_data = get_product_image(ASIN)
+        if not image_data:
+            return "Image data not found", 404
+
+        # send_file関数を使用して画像データを直接返す
+        return send_file(io.BytesIO(image_data), mimetype='image/jpeg')
+
+    except Exception as e:
+        app.logger.error(f"Error occurred: {e}")
+        return "Internal Server Error", 500
+
+
+@app.route('/get-details/<ASIN>')
+def get_details_endpoint(ASIN):
+    try:
+        product_name, product_description = get_product_details(ASIN)
+        product_price = get_current_price(ASIN)
+
+        if not product_name or not product_description or not product_price:
+            return "Product details not found", 404
+        
+        return jsonify(product_name=product_name, product_description=product_description, product_price=product_price)
+    except Exception as e:
+        app.logger.error(f"Error occurred: {e}")
+        return "Internal Server Error", 500
+
+
+@app.route("/price-history/<ASIN>")
+def price_history(ASIN):
+    response = api.query(ASIN, domain='JP', stats=180, history=True, to_datetime=True)
+    new_price_data = response[0]['data']['NEW']
+    new_price_times = response[0]['data']['NEW_time']
+    
+    # 販売日と価格を結合
+    price_history = list(zip(new_price_times, new_price_data))
+
+    # NaN or -0.01 を除外
+    price_history = [(date, price*100) for date, price in price_history if not isinstance(price, str) and price != -0.01 and price is not None and not (isinstance(price, float) and math.isnan(price))]
+
+    # 日付と価格に分割
+    dates, prices = zip(*price_history)
+
+    # 最新の日付を取得
+    latest_date = max(dates)
+
+    # 最新の日から180日前の日付を取得
+    days_ago = latest_date - datetime.timedelta(days=180)
+
+    # 180日前からの価格データをフィルタリング
+    filtered_dates = [date for date in dates if date >= days_ago]
+    filtered_prices = prices[-len(filtered_dates):]
+
+    data = {
+        "x": filtered_dates,
+        "y": filtered_prices
+    }
+    return jsonify(data)
+
+
+####################################################################################
+#Amazonの商品リンクを短縮
+####################################################################################
+
 def shorten_amazon_link(url):
-    # /dp/ パスのチェック
     dp_match = re.search('/dp/([A-Z0-9]+)', url)
     if dp_match:
-        asin = dp_match.group(1)
-        return "https://www.amazon.co.jp/dp/"+asin
+        ASIN = dp_match.group(1).upper()
+        return "https://www.amazon.co.jp/dp/"+ASIN
+    else:
+        return "amazon.co.jpの商品リンクではありません"
 
-    # /gp/product/ が含まれる商品もあるらしい（未確認） 少なそうなら消す予定
-    gp_match = re.search('/gp/product/([A-Z0-9]+)', url)
-    if gp_match:
-        asin = gp_match.group(1)
-        return "https://www.amazon.co.jp/dp/"+asin
+####################################################################################
+#商品画像の取得
+####################################################################################
 
-    # 上記のいずれもマッチしない場合
-    return "amazon.co.jpの商品リンクではありません"
+def get_product_image(ASIN):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    url = "https://www.amazon.co.jp/dp/" + ASIN
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        app.logger.error(f"Failed to fetch Amazon page. HTTP status: {response.status_code}")
+        return None
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    element_with_id = soup.find(id="landingImage")
+
+    if not element_with_id or not element_with_id.has_attr("src"):
+        app.logger.error(f"Image element not found for ASIN {ASIN}")
+        return None
+
+    img_url = element_with_id["src"]
+    response = requests.get(img_url)
+
+    if response.status_code != 200:
+        app.logger.error(f"Failed to fetch image. HTTP status: {response.status_code}")
+        return None
+    return response.content
+
+####################################################################################
+#商品名と商品概要、現在価格の取得
+####################################################################################
+
+def get_product_details(ASIN):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    url = "https://www.amazon.co.jp/dp/" + ASIN
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        app.logger.error(f"Failed to fetch Amazon page. HTTP status: {response.status_code}")
+        return None
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # 商品名を取得
+    title_element = soup.find(id="productTitle")
+    if not title_element:
+        app.logger.error(f"Product title element not found for ASIN {ASIN}")
+        return None
+    product_name = title_element.text.strip()
+
+    # 商品の概要を取得
+    about_element = soup.find(id="feature-bullets")
+    if not about_element:
+        app.logger.error(f"Product description element not found for ASIN {ASIN}")
+        return None
+    product_description = " ".join(li.text.strip() for li in about_element.find_all("li"))
+
+    return product_name, product_description
+
+def get_current_price(ASIN):
+    response = api.query(ASIN, domain='JP', history=True)
+    new_price_data = response[0]['data']['NEW']
+
+    # NaN or -0.01 を除外して最後の価格（現在の価格）を取得
+    current_price = next((price * 100 for price in reversed(new_price_data) if not isinstance(price, str) and price != -0.01 and price is not None and not (isinstance(price, float) and math.isnan(price))), None)
+
+    return current_price
+
+####################################################################################
+#Flaskアプリの起動
+####################################################################################
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
--- static/script.js
+++ static/script.js
@@ -1,23 +1,166 @@
-function copyToClipboard() {
-  const linkField = document.getElementById('shortenedLinkField');
-  navigator.clipboard.writeText(linkField.value);
-  alert('リンクをコピーしました！');
+// 短縮アドレスのコピー
+async function copyToClipboard() {
+  const copyText = document.getElementById('shortenedLinkField');
+  try {
+    await navigator.clipboard.writeText(copyText.value);
+    alert('リンクがコピーされました');
+  } catch (err) {
+    console.error('Failed to copy text: ', err);
+  }
 }
 
-document.getElementById("linkForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  
-  let amazonLink = document.getElementById("amazon_link").value;
-  
-  fetch('/', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: 'amazon_link=' + encodeURIComponent(amazonLink)
-  })
-  .then(response => response.json())
-  .then(data => {
-      document.getElementById("shortenedLinkField").value = data.shortened_link;
+document.getElementById("product_image").onload = function() {
+  document.getElementById("downloadButton").classList.remove("hidden");
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  /////////////////////////////////////////////////////
+  // グラフのデータを設定
+  const priceData = [
+    { x: [], y: [], type: 'scatter', mode: 'lines' }
+  ];
+
+  // 動的アノテーションを追加
+  const plotlyGraph = document.getElementById('plotlyGraph');
+  const annotation = {
+    xref: 'x',
+    yref: 'y',
+    ax: 0,
+    ay: -20,
+    bgcolor: 'rgba(255, 255, 255, 0.8)',
+    arrowhead: 4,
+    arrowsize: 1,
+    arrowwidth: 2,
+    arrowcolor: '#636363',
+    showarrow: true,
+  };
+
+  plotlyGraph.addEventListener('plotly_hover', (data) => {
+    const point = data.points[0];
+    const price = point.y;
+
+    annotation.x = point.x;
+    annotation.y = point.y;
+    annotation.text = `￥${price.toLocaleString()} 円`;
+
+    console.log("Annotation text:", annotation.text);
+
+    console.log('Relayout called');  // ここに追加
+    Plotly.relayout('plotlyGraph', { annotations: [annotation] });
+  });
+
+  plotlyGraph.addEventListener('plotly_unhover', () => {
+    Plotly.relayout('plotlyGraph', { annotations: [] });
+  });
+
+
+  /////////////////////////////////////////////////////
+  // テキストエリアの消去
+  document.getElementById('amazon_link').addEventListener('click', function() {
+    if (this.getAttribute('data-first-click') === 'true') {
+      this.value = '';
+      this.setAttribute('data-first-click', 'false');
+    }
+  });
+
+  document.getElementById("linkForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let amazonLink = document.getElementById("amazon_link").value;
+
+    ///////////////////////////////////////////////////////
+    //fetch
+    fetch('/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'amazon_link=' + encodeURIComponent(amazonLink)
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("shortenedLinkField").value = data.shortened_link;
+      
+        // ASINを取得して画像のURLを生成
+        const asinMatch = data.shortened_link.match(/\/dp\/([A-Z0-9]+)/);
+        if(asinMatch) {
+            const ASIN = asinMatch[1];
+            const imgURL = `/get-image/${ASIN}`;
+            document.getElementById("product_image").src = imgURL;
+
+            const productImageElement = document.getElementById("product_image");
+            productImageElement.style.display = 'block';
+
+          // 商品名と商品概要と現在価格の取得
+          fetch(`/get-details/${ASIN}`)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById("nameField").value = data.product_name;
+                document.getElementById("aboutField").value = data.product_description;
+                document.getElementById("priceField").value = data.product_price + "円";
+              })
+              .catch(error => {
+                  console.error('Error fetching product details:', error);
+                  //BeautifulSoupのエラー
+                  //KeepaAPPIのエラー　どちらかわかるようにするか？
+              });
+          // 価格履歴データを取得
+          fetch(`/price-history/${ASIN}`)
+              .then(response => response.json())
+              .then(data => {
+            
+                // Create date objects from the valid timestamps
+                priceData[0].x = data.x.map(dateStr => new Date(dateStr));
+                priceData[0].y = data.y;
+            
+                const timestamps = priceData[0].x.map(dateObj => dateObj.getTime());
+            
+                // Find min and max timestamps
+                const minTimestamp = Math.min(...timestamps);
+                const maxTimestamp = Math.max(...timestamps);
+            
+                // 文字列に変換
+                const minDate = new Date(minTimestamp).toISOString();
+                const maxDate = new Date(maxTimestamp).toISOString();
+            
+                // グラフのレイアウトを設定
+                const layout = {
+                  title: '価格推移',
+                  annotations: [],
+                  xaxis: {
+                    title: '日付',
+                    type: 'date',
+                    tickformat: "%Y-%m",
+                    range: [minDate, maxDate]
+                  },
+                  yaxis: {
+                    title: '価格 (円)',
+                    tickformat: ',.0f 円' // 追加分
+                  },
+                  width: 1000,
+                  height: 400
+                  // autosize: true, // 自動サイズ調整を有効にする
+                  // responsive: true, // レスポンシブデザインを有効にする
+                };
+
+                priceData[0].line = {
+                  shape: 'hv', // ステップグラフの形状を設定
+                  color: 'green', // 線の色を設定
+                  width: 2      // 線の太さを設定
+                };
+            
+                // priceData[0].opacity = 1.0; // 透明度を設定 (0.0から1.0の範囲)
+            
+                priceData[0].fill = 'tozeroy';
+                priceData[0].fillcolor = 'rgba(144, 206, 156, 0.3)';
+            
+                layout.xaxis.range = [minDate, maxDate];
+                layout.yaxis.range = [Math.min(...data.y), Math.max(...data.y)];
+            
+                Plotly.newPlot('plotlyGraph', priceData, layout);
+              });
+        }
+    });
   });
 });
